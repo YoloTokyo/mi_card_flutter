be6aa380e8f9f3a35175e94af25ddebbd50761c7
YoloChoppa <yolo.hsu@gmail.com>
2025-07-28 18:45:21 +0900

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,37 +13,73 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Container(
-              width: 100.0,
-              height: 100.0,
-              color: Colors.red,
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-              Container(
-                width: 100.0,
-                height: 100.0,
-                color: Colors.yellow,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                //control+j getting quick doc
+                radius: 70.0,
+                backgroundColor: Colors.red,
+                backgroundImage: AssetImage('images/chopa.jpeg'),
               ),
-              Container(
-                width: 100.0,
-                height: 100.0,
-                color: Colors.yellow[100],
+              Text(
+                'Chopper',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                'Bristol App Solutions Founder',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                  fontFamily: 'Source Sans Pro',
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone_enabled,
+                    size: 30.0,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+81 904 605 715',
+                  ),
+                ),
+              ),
+              Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      size: 30.0,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'jude-jude-chopper@gmail.com',
+                    ),
+                  ),
               ),
             ],
-            ),
-            Container(
-              width: 100.0,
-              height: 100.0,
-              color: Colors.blue,
-            )
-          ],
-        ),
+          ),
         ),
       ),
     );
