54b1dde03177ad28042131e07bf4ab9eac5b5b53
YoloChoppa <yolo.hsu@gmail.com>
2025-07-28 16:36:19 +0900

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+              width: 100.0,
+              height: 100.0,
+              color: Colors.red,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.yellow,
+              ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.yellow[100],
+              ),
+            ],
+            ),
+            Container(
+              width: 100.0,
+              height: 100.0,
+              color: Colors.blue,
+            )
+          ],
+        ),
+        ),
+      ),
+    );
+  }
+}
